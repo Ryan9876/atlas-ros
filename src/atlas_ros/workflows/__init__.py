2f885b1d95e55fbd7c0841cfbec334311909eab1
Ryan9876 <ryansmith100@gmail.com>
2026-07-21 21:52:15 -0400

    P0: route production W04 through trust guard layer

--- src/atlas_ros/workflows/__init__.py
+++ src/atlas_ros/workflows/__init__.py
@@ -2,7 +2,7 @@ from .w01_capture import CaptureService
 from .w02_routing import RoutingService
 from .w03_todoist import TodoistService
 from .w03a_decomposition import DecompositionService
-from .w04_reconciliation import TodoistReconciliationService
+from .w04_trust import TodoistReconciliationService
 
 __all__ = [
     "CaptureService",
