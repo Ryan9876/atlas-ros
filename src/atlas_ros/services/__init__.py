f8ad589b035761dce3142ccd643645bef30d343a
Ryan9876 <ryansmith100@gmail.com>
2026-07-24 09:51:15 -0400

    phase6: export execution reconciliation service

--- src/atlas_ros/services/__init__.py
+++ src/atlas_ros/services/__init__.py
@@ -1,7 +1,10 @@
+from .execution_reconciliation import ExecutionReconciliationService, ReconciliationOutcome
 from .record_routing import RecordRoutingService, RoutingDecision
 from .routing_shadow import RoutingDifferential, RoutingShadowComparator
 
 __all__ = [
+    "ExecutionReconciliationService",
+    "ReconciliationOutcome",
     "RecordRoutingService",
     "RoutingDecision",
     "RoutingDifferential",
