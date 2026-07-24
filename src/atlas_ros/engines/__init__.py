deaafddd9e24efacc4d315e60a03e7ccacb83111
Ryan9876 <ryansmith100@gmail.com>
2026-07-24 09:00:58 -0400

    Export knowledge and management engines

--- src/atlas_ros/engines/__init__.py
+++ src/atlas_ros/engines/__init__.py
@@ -1,3 +1,9 @@
+from .knowledge_composition import KnowledgeCompositionEngine
 from .management_reasoning import ManagementReasoningEngine
+from .management_structure import ManagementStructureEngine
 
-__all__ = ["ManagementReasoningEngine"]
+__all__ = [
+    "KnowledgeCompositionEngine",
+    "ManagementReasoningEngine",
+    "ManagementStructureEngine",
+]
