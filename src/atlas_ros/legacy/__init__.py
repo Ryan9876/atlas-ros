fd6b1c2c3a56244322fceecc80991a9f195d7871
Ryan9876 <ryansmith100@gmail.com>
2026-07-24 01:40:12 -0400

    Export legacy compatibility facades

---
+++ src/atlas_ros/legacy/__init__.py
@@ -0,0 +1,13 @@
+from .facades import (
+    W01CaptureFacade,
+    W02RoutingFacade,
+    W03ADecompositionFacade,
+    W03TodoistFacade,
+)
+
+__all__ = [
+    "W01CaptureFacade",
+    "W02RoutingFacade",
+    "W03ADecompositionFacade",
+    "W03TodoistFacade",
+]
