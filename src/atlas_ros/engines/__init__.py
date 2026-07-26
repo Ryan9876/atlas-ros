555389642b42f8d2d837e3f4c9b0c681b16ee06b
Ryan9876 <ryansmith100@gmail.com>
2026-07-26 13:17:27 -0400

    Route management planning through coherence integration

--- src/atlas_ros/engines/__init__.py
+++ src/atlas_ros/engines/__init__.py
@@ -1,8 +1,8 @@
 from .classification_explainability import ClassificationExplainability
+from .coherent_management_structure import ManagementStructureEngine
 from .intent_partitioning import IntentPartitioner
 from .knowledge_composition import KnowledgeCompositionEngine
 from .management_reasoning import ManagementReasoningEngine
-from .management_structure import ManagementStructureEngine
 from .manager_intent import IntentAssessment, ManagerIntentInferer
 from .reasoning_coherence import ReasoningCoherenceGate
 from .responsibility_classification import ResponsibilityAssessment, ResponsibilityClassifier
