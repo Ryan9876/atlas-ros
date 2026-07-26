9b5ccd4246f940ef12273f50abbd028359dd392c
Ryan9876 <ryansmith100@gmail.com>
2026-07-26 18:51:03 -0400

    Export v6.2 adaptive input-processing engines

--- src/atlas_ros/engines/__init__.py
+++ src/atlas_ros/engines/__init__.py
@@ -1,5 +1,26 @@
+from .archetypes_v62 import (
+    ArchetypeRegistryV62,
+    ArchetypeSelectionEngineV62,
+    CanonicalIntentEngineV62,
+    MultiOutcomeEngineV62,
+)
 from .classification_explainability import ClassificationExplainability
 from .coherent_management_structure import ManagementStructureEngine
+from .decision_support_v62 import (
+    AdaptiveProjectionEngineV62,
+    ClarificationEngineV62,
+    ConfidenceProfileEngineV62,
+    PlanningMemoryEngineV62,
+    PlanningStyleEngineV62,
+    ReflectionGateV62,
+    RiskProfileEngineV62,
+)
+from .input_pipeline_v62 import AdaptiveInputProcessingPipelineV62
+from .intent_graph_v62 import (
+    ConstraintPropagationEngineV62,
+    DependencyDiscoveryEngineV62,
+    IntentGraphEngineV62,
+)
 from .intent_partitioning import IntentPartitioner
 from .knowledge_composition import KnowledgeCompositionEngine
 from .management_reasoning import ManagementReasoningEngine
@@ -8,6 +29,21 @@ from .reasoning_coherence import ReasoningCoherenceGate
 from .responsibility_classification import ResponsibilityAssessment, ResponsibilityClassifier
 
 __all__ = [
+    "AdaptiveInputProcessingPipelineV62",
+    "AdaptiveProjectionEngineV62",
+    "ArchetypeRegistryV62",
+    "ArchetypeSelectionEngineV62",
+    "CanonicalIntentEngineV62",
+    "ClarificationEngineV62",
+    "ConfidenceProfileEngineV62",
+    "ConstraintPropagationEngineV62",
+    "DependencyDiscoveryEngineV62",
+    "IntentGraphEngineV62",
+    "MultiOutcomeEngineV62",
+    "PlanningMemoryEngineV62",
+    "PlanningStyleEngineV62",
+    "ReflectionGateV62",
+    "RiskProfileEngineV62",
     "ClassificationExplainability",
     "IntentAssessment",
     "IntentPartitioner",
