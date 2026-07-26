a4c04d327832afd9a271af8597eb7b922179d038
Ryan9876 <ryansmith100@gmail.com>
2026-07-26 12:55:25 -0400

    Export coherence and horizon engines

--- src/atlas_ros/engines/__init__.py
+++ src/atlas_ros/engines/__init__.py
@@ -1,12 +1,15 @@
 from .classification_explainability import ClassificationExplainability
+from .horizon_promotion import AttendedHorizonPromotionEngine
 from .intent_partitioning import IntentPartitioner
 from .knowledge_composition import KnowledgeCompositionEngine
 from .management_reasoning import ManagementReasoningEngine
 from .management_structure import ManagementStructureEngine
 from .manager_intent import IntentAssessment, ManagerIntentInferer
+from .reasoning_coherence import ReasoningCoherenceGate
 from .responsibility_classification import ResponsibilityAssessment, ResponsibilityClassifier
 
 __all__ = [
+    "AttendedHorizonPromotionEngine",
     "ClassificationExplainability",
     "IntentAssessment",
     "IntentPartitioner",
@@ -14,6 +17,7 @@ __all__ = [
     "ManagementReasoningEngine",
     "ManagementStructureEngine",
     "ManagerIntentInferer",
+    "ReasoningCoherenceGate",
     "ResponsibilityAssessment",
     "ResponsibilityClassifier",
 ]
