2f0be7edf51f01e83f248e49cdeb17ed5f70ef45
Ryan9876 <ryansmith100@gmail.com>
2026-07-27 14:03:40 -0400

    feat(v7): expose kernel authority primitives

---
+++ src/atlas_ros/kernel/__init__.py
@@ -0,0 +1,6 @@
+"""Atlas ROS v7 composition-root primitives."""
+
+from atlas_ros.kernel.authority import AuthorityRecord
+from atlas_ros.kernel.digests import sha256_digest
+
+__all__ = ["AuthorityRecord", "sha256_digest"]
