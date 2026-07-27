e0a959d601972030c83f4f4fc72541b408a3a732
Ryan9876 <ryansmith100@gmail.com>
2026-07-27 15:36:45 -0400

    feat: export attended execution service

--- src/atlas_ros/application/__init__.py
+++ src/atlas_ros/application/__init__.py
@@ -1,5 +1,10 @@
 """Application use-case coordination layer."""
 
 from atlas_ros.application.canonical_processing import CanonicalProcessingCoordinator
+from atlas_ros.application.execution import AttendedExecutionService, ExecutionBoundaryError
 
-__all__ = ["CanonicalProcessingCoordinator"]
+__all__ = [
+    "AttendedExecutionService",
+    "CanonicalProcessingCoordinator",
+    "ExecutionBoundaryError",
+]
