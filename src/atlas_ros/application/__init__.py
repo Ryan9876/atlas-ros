98cb81359676a69873deba07b25e1efeff1ecec5
Ryan9876 <ryansmith100@gmail.com>
2026-07-27 14:04:02 -0400

    feat(v7): introduce application layer

---
+++ src/atlas_ros/application/__init__.py
@@ -0,0 +1,5 @@
+"""Application use-case coordination layer."""
+
+from atlas_ros.application.canonical_processing import CanonicalProcessingCoordinator
+
+__all__ = ["CanonicalProcessingCoordinator"]
