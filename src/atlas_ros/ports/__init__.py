694b0e01984e77bacb6e9613eb27ed464dac4efe
Ryan9876 <ryansmith100@gmail.com>
2026-07-27 15:36:33 -0400

    feat: export provider execution port

--- src/atlas_ros/ports/__init__.py
+++ src/atlas_ros/ports/__init__.py
@@ -1,5 +1,6 @@
 """Provider-neutral boundary interfaces."""
 
 from atlas_ros.ports.authority import AuthorityReader
+from atlas_ros.ports.execution import ProviderExecutionPort
 
-__all__ = ["AuthorityReader"]
+__all__ = ["AuthorityReader", "ProviderExecutionPort"]
