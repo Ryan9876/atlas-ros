45063996702ec07233e81c3a5547a6fb3606127c
Ryan9876 <ryansmith100@gmail.com>
2026-07-27 16:35:06 -0400

    refactor: export execution durability and permission ports

--- src/atlas_ros/ports/__init__.py
+++ src/atlas_ros/ports/__init__.py
@@ -1,6 +1,16 @@
 """Provider-neutral boundary interfaces."""
 
 from atlas_ros.ports.authority import AuthorityReader, DynamicAuthorityReader
-from atlas_ros.ports.execution import ProviderExecutionPort
+from atlas_ros.ports.execution import (
+    ExecutionJournalPort,
+    ProviderExecutionPort,
+    ProviderWriteGuard,
+)
 
-__all__ = ["AuthorityReader", "DynamicAuthorityReader", "ProviderExecutionPort"]
+__all__ = [
+    "AuthorityReader",
+    "DynamicAuthorityReader",
+    "ExecutionJournalPort",
+    "ProviderExecutionPort",
+    "ProviderWriteGuard",
+]
