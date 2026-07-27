935a47e64caa4d186ec63a2c5edb905b2c07b857
Ryan9876 <ryansmith100@gmail.com>
2026-07-27 16:23:14 -0400

    feat(v7): export attended authorization service

--- src/atlas_ros/application/__init__.py
+++ src/atlas_ros/application/__init__.py
@@ -1,9 +1,14 @@
 """Application use-case coordination layer."""
 
 from atlas_ros.application.canonical_processing import CanonicalProcessingCoordinator
-from atlas_ros.application.execution import AttendedExecutionService, ExecutionBoundaryError
+from atlas_ros.application.execution import (
+    AttendedAuthorizationService,
+    AttendedExecutionService,
+    ExecutionBoundaryError,
+)
 
 __all__ = [
+    "AttendedAuthorizationService",
     "AttendedExecutionService",
     "CanonicalProcessingCoordinator",
     "ExecutionBoundaryError",
