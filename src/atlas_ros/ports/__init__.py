669c18773a74281120d7eedfcae64e0a2c63faa2
Ryan9876 <ryansmith100@gmail.com>
2026-07-27 15:38:15 -0400

    feat: export dynamic authority reader

--- src/atlas_ros/ports/__init__.py
+++ src/atlas_ros/ports/__init__.py
@@ -1,6 +1,6 @@
 """Provider-neutral boundary interfaces."""
 
-from atlas_ros.ports.authority import AuthorityReader
+from atlas_ros.ports.authority import AuthorityReader, DynamicAuthorityReader
 from atlas_ros.ports.execution import ProviderExecutionPort
 
-__all__ = ["AuthorityReader", "ProviderExecutionPort"]
+__all__ = ["AuthorityReader", "DynamicAuthorityReader", "ProviderExecutionPort"]
