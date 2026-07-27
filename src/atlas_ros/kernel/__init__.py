7945d8b10df87ce73db8888aa23f697095bcd355
Ryan9876 <ryansmith100@gmail.com>
2026-07-27 15:51:38 -0400

    fix: export v7 runtime kernel public contract

--- src/atlas_ros/kernel/__init__.py
+++ src/atlas_ros/kernel/__init__.py
@@ -1,6 +1,13 @@
 """Atlas ROS v7 composition-root primitives."""
 
 from atlas_ros.kernel.authority import AuthorityRecord
+from atlas_ros.kernel.container import KernelConfig, RuntimeKernel, RuntimeMode
 from atlas_ros.kernel.digests import sha256_digest
 
-__all__ = ["AuthorityRecord", "sha256_digest"]
+__all__ = [
+    "AuthorityRecord",
+    "KernelConfig",
+    "RuntimeKernel",
+    "RuntimeMode",
+    "sha256_digest",
+]
