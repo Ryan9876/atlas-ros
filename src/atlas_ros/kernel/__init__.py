43d6e9baf2623911d2cb2d767e1e222bc92814df
Ryan9876 <ryansmith100@gmail.com>
2026-07-27 16:03:18 -0400

    feat: export governed kernel errors

--- src/atlas_ros/kernel/__init__.py
+++ src/atlas_ros/kernel/__init__.py
@@ -1,12 +1,20 @@
 """Atlas ROS v7 composition-root primitives."""
 
 from atlas_ros.kernel.authority import AuthorityRecord
-from atlas_ros.kernel.container import KernelConfig, RuntimeKernel, RuntimeMode
+from atlas_ros.kernel.container import (
+    KernelConfig,
+    KernelConfigurationError,
+    KernelPermissionError,
+    RuntimeKernel,
+    RuntimeMode,
+)
 from atlas_ros.kernel.digests import sha256_digest
 
 __all__ = [
     "AuthorityRecord",
     "KernelConfig",
+    "KernelConfigurationError",
+    "KernelPermissionError",
     "RuntimeKernel",
     "RuntimeMode",
     "sha256_digest",
