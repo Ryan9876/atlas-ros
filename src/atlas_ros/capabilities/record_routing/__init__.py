1223fd9e96fb8334d0c0cf33442c7b4eb2f17b7b
Ryan9876 <ryansmith100@gmail.com>
2026-07-27 16:19:22 -0400

    feat(v7): add record routing capability boundary

---
+++ src/atlas_ros/capabilities/record_routing/__init__.py
@@ -0,0 +1,7 @@
+"""Record routing capability boundary for Atlas ROS v7."""
+
+from atlas_ros.capabilities.interfaces import RecordRoutingPort, RoutingDecision
+
+CAPABILITY_ID = "atlas.record-routing"
+
+__all__ = ["CAPABILITY_ID", "RecordRoutingPort", "RoutingDecision"]
