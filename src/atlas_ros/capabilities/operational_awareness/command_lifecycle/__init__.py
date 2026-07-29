a62585b8c45d4d33f93be43bd83526b1302b844b
Ryan9876 <ryansmith100@gmail.com>
2026-07-28 20:48:56 -0400

    fix: declare command-lifecycle capability identity

--- src/atlas_ros/capabilities/operational_awareness/command_lifecycle/__init__.py
+++ src/atlas_ros/capabilities/operational_awareness/command_lifecycle/__init__.py
@@ -1,7 +1,10 @@
 from .parser import AtlasCommandParser, CommandParseError
 from .planner import CommandLifecycleService, LifecyclePlanningError
 
+CAPABILITY_ID = "atlas.command-lifecycle"
+
 __all__ = [
+    "CAPABILITY_ID",
     "AtlasCommandParser",
     "CommandLifecycleService",
     "CommandParseError",
